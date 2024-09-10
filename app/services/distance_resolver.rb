74bd86f39d62f9fa0f27701795354bc8f55d195a
Edyta Barańska <edytacr@gmail.com>
2024-09-10 10:48:40 +0200

    secure scenario for distance calculations when location is not exist

--- app/services/distance_resolver.rb
+++ app/services/distance_resolver.rb
@@ -4,6 +4,8 @@ class DistanceResolver
   end
 
   def perform
+    return 0 if locations_not_founded?
+
     Geocoder::Calculations.distance_between(start_coordinates(trip), destination_coordinates(trip))
   end
 
@@ -13,11 +15,17 @@ class DistanceResolver
 
   def start_coordinates(trip)
     start_location = Location.find_by(address: trip.start_address)
-    [start_location.latitude,start_location.longitude]
+    
+    start_location.nil? ? nil : [start_location.latitude,start_location.longitude]
   end
 
   def destination_coordinates(trip)
     destination_location = Location.find_by(address: trip.destination_address)
-    [destination_location.latitude,destination_location.longitude]
+
+    destination_location.nil? ? nil : [destination_location.latitude,destination_location.longitude]
+  end
+
+  def locations_not_founded?
+    start_coordinates(trip).nil? || destination_coordinates(trip).nil?
   end
 end
