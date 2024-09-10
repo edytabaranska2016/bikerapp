ad0f1165fc38609748a156d860269b328ab3b30f
Edyta Barańska <edytacr@gmail.com>
2024-09-10 10:14:25 +0200

    trip model validations

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,2 +1,12 @@
 class Trip < ApplicationRecord
+  validates :start_address, presence: true
+  validates :start_address, format: { with: /.{3,}\, .{3,}\, .{3,}/,
+    message: "address should be given as street (plus number), city, country separated by commas" }
+  validates :destination_address, presence: true
+  validates :destination_address, format: { with: /.{3,}\, .{3,}\, .{3,}/,
+    message: "address should be given as street (plus number), city, country separated by commas" }
+  validates :price, presence: true
+  validates :price, comparison: { greater_than_or_equal_to: 0, message: "price cannot be negative" }
+  validates :date, presence: true
+  validates :date, format: { with: /\d{4}\-\d{2}\-\d{2}/ , message: "date should be in format YYYY-MM-DD" }
 end
