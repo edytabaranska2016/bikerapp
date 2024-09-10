6494d9a8e281a9ca7f97c401f7ed9eb3a97a9eac
Edyta Barańska <edytacr@gmail.com>
2024-09-10 13:13:45 +0200

    simplify weekly stats resolver

--- app/services/weekly_stats_resolver.rb
+++ app/services/weekly_stats_resolver.rb
@@ -13,21 +13,13 @@ class WeeklyStatsResolver
   end
 
   def calculated_total_distance
-    total_distance = 0
-    current_week_trips_scope.each do |trip|
-      geocoded_distance = ::DistanceResolver.new(trip).perform
-      total_distance += geocoded_distance
-    end
+    total_distance = current_week_trips_scope.sum(:distance)
 
     "#{total_distance.round}km"
   end
 
   def calculated_total_price
-    total_price = 0
-
-    current_week_trips_scope.each do |trip|
-      total_price += trip.price
-    end
+    total_price = current_week_trips_scope.sum(:price)
 
     "#{total_price.round(2)}PLN"
   end
